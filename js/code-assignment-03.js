3f8231b128fc47b5f991791d1f9ee04103fa5ca1
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-15 11:46:14 -0600

    2/15/18

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,3 +1,28 @@
+// assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectTitle = "Earthquakes"
-let
+let projectUrl = 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d'
+let projectTitle = 'Map of Recent Earthquakes'
+let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
+let projectThumb = true
+let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
+
+let projectArray = [
+  1,
+  'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d',
+  'Map of Recent Earthquakes',
+  'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.',
+  true
+  ['earthquakes','usgs']
+]
+
+let projectObject = {
+  id: 1,
+  url: 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d'
+  title: 'Map of Recent Earthquakes'
+  description:'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
+  hasThumbnail: true
+  keywords: ['earthquakes', 'usgs']
+}
+
+// display messages in the browser's developer console, combining the values with text
+console.log(projectObject)
