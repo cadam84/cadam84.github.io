a654afcc26d8a9b5cc566225281a999dd6e86668
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-20 10:17:09 -0600

    change url in code 3

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,6 +1,6 @@
 // assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectUrl = 'https://arcg.is/0bXqvu'
+let projectUrl = 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990'
 let projectTitle = 'Book Assignment 2 Earthquakes'
 let projectDesc = 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.'
 let projectThumb = true
@@ -9,7 +9,7 @@ let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
 // display messages in the browser's developer console, combining the values with text
 console.log('ID: ' + 1)
-console.log('URL: ' + 'https://arcg.is/0bXqvu')
+console.log('URL: ' + 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990u')
 console.log('Title: ' + 'Book Assignment 2 Earthquakes')
 console.log('Description: ' + 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.')
 console.log('Has thumbnail? ' + true)
