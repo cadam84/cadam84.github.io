6c1d658d60b0094ba40ec5df5b2ab60a55e12ebc
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-19 22:13:43 -0600

    code 3 update

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,27 +1,27 @@
 // assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectUrl = 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d'
-let projectTitle = 'Map of Recent Earthquakes'
-let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
+let projectUrl = 'https://arcg.is/0bXqvu'
+let projectTitle = 'Book Assignment 2 Earthquakes'
+let projectDesc = 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.'
 let projectThumb = true
 let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
 let projectArray = [
   1,
-  'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d',
-  'Map of Recent Earthquakes',
-  'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.',
+  'https://arcg.is/0bXqvu',
+  'Book Assignment 2 Earthquakes',
+  'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.',
   true
-  ['earthquakes','usgs']
+  ['earthquakes','USGS']
 ]
 
 let projectObject = {
   id: 1,
-  url: 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d'
+  url: 'https://arcg.is/0bXqvu'
   title: 'Map of Recent Earthquakes'
-  description:'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
+  description:'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.'
   hasThumbnail: true
-  keywords: ['earthquakes', 'usgs']
+  keywords: ['earthquakes', 'USGS']
 }
 
 // display messages in the browser's developer console, combining the values with text
