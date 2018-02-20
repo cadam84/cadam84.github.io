8a2e8fe16a9c2f6654a2f0d15aab76bc7782e31b
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-20 10:11:30 -0600

    still dumb

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -10,7 +10,7 @@ let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 // display messages in the browser's developer console, combining the values with text
 console.log('ID: ' + 1)
 console.log('URL: ' + 'https://arcg.is/0bXqvu')
-console.log('Title: ' 'Book Assignment 2 Earthquakes')
+console.log('Title: ' + 'Book Assignment 2 Earthquakes')
 console.log('Description: ' + 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.')
 console.log('Has thumbnail? ' + true)
 console.log('Map extent: ' + [48.51, -28.37, 2.63, -132.34])
