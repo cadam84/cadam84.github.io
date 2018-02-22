2882cfd00fc7c1817ca0713393d199d041acc968
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-22 11:02:56 -0600

    js4 & pic folder

--- js/code-assignment-04.js
+++ js/code-assignment-04.js
@@ -4,26 +4,26 @@ let projectUrl = 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada72
 let projectTitle = 'Book Assignment 2 Earthquakes'
 let projectDesc = 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.'
 let projectThumb = true
-let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
+let projectKeywords = ['USGS', 'Earthquakes', 'Book2' ]
 
 // create an array and assign six variables to positions 0 through 5
 let projectArray = [
-  projectId, 1
-  projectUrl, 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990'
-  projectTitle, 'Book Assignment 2 Earthquakes'
-  projectDesc, 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.'
-  projectThumb, true
-  projectMapExtent, [48.51, -28.37, 2.63, -132.34]
+  projectId, 1,
+  projectUrl, 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990',
+  projectTitle, 'Book Assignment 2 Earthquakes',
+  projectDesc, 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.',
+  projectThumb, true,
+  projectKeywords, ['USGS', 'Earthquakes', 'Book2']
 ]
 
 // create an object and assign six variables to property names (i.e., keys)
 let projectObject = {
-  id: projectId, 1
-  url: projectUrl, 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990'
-  title: projectTitle, 'Book Assignment 2 Earthquakes'
-  desc: projectDesc, 'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.'
-  thumb: projectThumb, true
-  projectMapExtent: projectMapExtent [48.51, -28.37, 2.63, -132.34]
+  id: 1,
+  url: 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990',
+  title:'Book Assignment 2 Earthquakes',
+  desc:'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.',
+  thumb: true,
+  projectKeywords: ['USGS', 'Earthquakes', 'Book2']
 }
 
 // logs the title from the project data array, because the title is in position 2
