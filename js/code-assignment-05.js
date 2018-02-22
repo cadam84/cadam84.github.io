77144a858c5b9bf643c004bf2b171f053b73b0de
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-22 11:27:21 -0600

    code 5

---
+++ js/code-assignment-05.js
@@ -0,0 +1,22 @@
+// create an object and assign six variables to property names (i.e., keys)
+let project1 = {
+  id: 1,
+  url: 'http://arcg.is/2n7m5PD',
+  title:'Book Assignment 1 Nashville',
+  desc:'This is a story map tour of Nashville, Tennesse.',
+  thumb: true,
+  keywords: ['Nashville', 'story', 'map']
+}
+
+let project2 = {
+  id: 2,
+  url: 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990',
+  title:'Book Assignment 2 Earthquakes',
+  desc:'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.',
+  thumb: true,
+  keywords: ['USGS', 'Earthquakes', 'Book2']
+}
+
+let projects = [project1, project2]
+
+console.log(projects);
