4ea7fc21675ef76116f6ee7b56074a4039a530cc
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-02-27 10:56:05 -0600

    code 5 complete

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,20 +1,23 @@
-let projects = [ {
+let projectData = []
+
+projectData[0] = {
   id: 1,
   url: 'http://arcg.is/2n7m5PD',
   title:'Book Assignment 1 Nashville',
   desc:'This is a story map tour of Nashville, Tennesse.',
   thumb: true,
   keywords: ['Nashville', 'story', 'map']
-},
-{
+}
+
+projectData[1] = {
   id: 2,
   url: 'https://www.arcgis.com/apps/View/index.html?appid=6d3a5aeada7249c99c3dcc636ace0b80&extent=-479.5312,-89.5436,-119.5312,89.9990',
   title:'Book Assignment 2 Earthquakes',
   desc:'This is an up to date map of earthquakes across the world, magnitude 2.5+ provided by the USGS website.',
   thumb: false,
   keywords: ['USGS', 'Earthquakes', 'Book2']
-} ]
+}
 
-for (let i=0; i < projects.length; i++) {
-  console.log(i)
+for (let i = 0; i < projectData.length; i++) {
+  console.log('Images of Project' + projectData[i].id + ".png")
 }
