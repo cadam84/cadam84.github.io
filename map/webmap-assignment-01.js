1868c6432782a8a9ebe8b2e24ab19c075cbd2986
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-04-03 09:27:55 -0500

    webmap 1 ~ edit 2

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,16 +1,24 @@
-let map1 = L.map('webmap1').setView([36.113614, -86.718002], 10)
+let map1 = L.map('webmap1').setView([36.146193 , -86.745961], 12.5)
 let basemap = 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'
 L.tileLayer(basemap).addTo(map1)
-let marker = L.marker([36.113614, -86.718002]).addTo(map1)
+let marker = L.marker([36.1263, -86.6774]).addTo(map1)
 let polygon = L.polygon([
-  [36.193088, -86.716471],
-  [36.24073, -86.623087],
-  [36.227438, -86.667032]
+  [36.169962 , -86.774972],
+  [36.166602 , -86.778319],
+  [36.163795 , -86.772225]
 ]).addTo(map1);
 
-polygon.bindPopup('St. Katharine\'s and Wapping')
-marker.bindPopup('Nashville, Tennesse')
+polygon.bindPopup('Nissan Stadium and Public Square Park')
+marker.bindPopup('Nashville International Airport - BNA')
 
-map1.on('click', function (event) {
-  console.log('You clicked the map at ' + event.latlng)
+map1.on('touch', function (event) {
+  console.log('You touched the map of Nashville at ' + event.latlng)
 })
+
+
+var latlngs = [
+    [36.159523	-86.771058],
+    [36.161914	-86.769083],
+    [36.166072	-86.771058]
+];
+var polyline = L.polyline(latlngs, {color: 'green'}).addTo(map1);
