394198204e9d70d786ad0f87c14539896dbe1b52
cadam84 <35343374+cadam84@users.noreply.github.com>
2018-04-24 11:41:34 -0500

    project edit

--- map/webmap-finalproject.js
+++ map/webmap-finalproject.js
@@ -18,6 +18,12 @@ let mybasemaps = {
 
 L.control.layers(mybasemaps).addTo(map6)
 
+let markerIcon = L.icon({
+    iconUrl: 'marker-icon-orange.png',
+    iconSize: [10, 15],
+    })
+
+let markerOption = { icon: markerIcon }
 
 let marker1 = L.marker([36.182045,-86.7510467]).addTo(map6)
 let marker2 = L.marker([36.1580902,-86.851065]).addTo(map6)
@@ -25,11 +31,20 @@ let marker3 = L.marker([36.0840113,-86.7075445]).addTo(map6)
 let marker4 = L.marker([36.1685816,-86.7396784]).addTo(map6)
 let marker5 = L.marker([35.9322536,-86.8621235]).addTo(map6)
 
-let polygon = L.polygon([
+
+
+let polygonShape = [
   [36.182045,-86.7510467],
   [36.1580902,-86.851065],
   [36.1685816,-86.7396784]
-]).addTo(map6);
+]
+
+let polygonStyle = {
+  color: 'navy',
+  fillColor: '#b5d1ff',
+  fillOpacity: 0.5
+}
+let polygon = L.polygon(polygonShape, polygonStyle).addTo(map6)
 
 polygon.bindPopup('3 Main Butcher Shops in Central Nashville')
 marker1.bindPopup('Porter Road Butcher Shop')
@@ -37,7 +52,6 @@ marker2.bindPopup('Bare Bones Butcher Shop')
 marker3.bindPopup('Carniceria San Luis')
 marker4.bindPopup('Roys Meat Service')
 marker5.bindPopup('Carnivore Meat Company')
-
 
 
 map6.on('click', function (event) {
